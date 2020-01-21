5b4b9d9f8654ffa4306972084c7a4d16dfff0441
Anand Gautam <58807010+anand-9911@users.noreply.github.com>
2020-01-21 12:47:20 +0530

    removing keys

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 import tweepy
 import time
 
-consumer_key = 'k2e5R5td9urQHTrfmnBFjbNwT'
-consumer_secret = '03d63UAvmAjHKuVUmv1Dn6Mty2SeydkiBTcxcFm61SM3vEIzC2'
-access_token = '4234977798-yPmVbGqvTpabnZTIUY6nTb79p8XVXwm4Ois4gjx'
-access_token_secret = 'WwXIcaO47dWi6EsBNvBXY8lKD6PMy3dJSJlaiHNKv8OZg'
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
